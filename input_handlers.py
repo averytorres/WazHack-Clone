6cdc34b5c2033d682608c8c43685d1a11c69ab62
Avery Torres <atorres@scad.edu>
2019-06-18 09:40:12 -0400

    add collision detection

--- input_handlers.py
+++ input_handlers.py
@@ -15,7 +15,6 @@ def handle_keys(key):
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         # Alt+Enter: toggle full screen
         return {'fullscreen': True}
-
     elif key.vk == libtcod.KEY_ESCAPE:
         # Exit the game
         return {'exit': True}
