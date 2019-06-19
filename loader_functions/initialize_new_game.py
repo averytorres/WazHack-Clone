3318d1802d0777be2e6c9df6dda7007676db25cb
Avery Torres <atorres@scad.edu>
2019-06-19 07:34:02 -0400

    add variable item/monster generation

--- loader_functions/initialize_new_game.py
+++ loader_functions/initialize_new_game.py
@@ -1,10 +1,17 @@
 import tcod as libtcod
+
 from components.fighter import Fighter
 from components.inventory import Inventory
+from components.level import Level
+
 from entity import Entity
+
 from game_messages import MessageLog
+
 from game_states import GameStates
+
 from map_objects.game_map import GameMap
+
 from render_functions import RenderOrder
 
 
@@ -70,16 +77,16 @@ def get_constants():
 
 
 def get_game_variables(constants):
-    fighter_component = Fighter(hp=30, defense=2, power=5)
+    fighter_component = Fighter(hp=100, defense=1, power=4)
     inventory_component = Inventory(26)
+    level_component = Level()
     player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, render_order=RenderOrder.ACTOR,
-                    fighter=fighter_component, inventory=inventory_component)
+                    fighter=fighter_component, inventory=inventory_component, level=level_component)
     entities = [player]
 
     game_map = GameMap(constants['map_width'], constants['map_height'])
     game_map.make_map(constants['max_rooms'], constants['room_min_size'], constants['room_max_size'],
-                      constants['map_width'], constants['map_height'], player, entities,
-                      constants['max_monsters_per_room'], constants['max_items_per_room'])
+                      constants['map_width'], constants['map_height'], player, entities)
 
     message_log = MessageLog(constants['message_x'], constants['message_width'], constants['message_height'])
 
