943a2f7fa87d1f8a010e4b191acae49700c3ac4e
Avery Torres <averytorres.mail@gmail.com>
2019-06-21 23:17:02 -0400

    fix weapon choice

--- item_choice_definition/weapon_choices.py
+++ item_choice_definition/weapon_choices.py
@@ -9,13 +9,14 @@ from item_choice_definition.weapon_choices_values.sword_choices import get_sword
 def get_weapon_choice(item_choice, x, y):
     wp_en = '-weapon'
     disp_name = item_choice.replace(wp_en,"")
-
-    item = get_knife_choices(item_choice,wp_en,x,y,disp_name)
-
-    item = get_sword_choices(item_choice, wp_en, x, y, disp_name)
+    item = None
 
     if item is None:
+        item = get_knife_choices(item_choice,wp_en,x,y,disp_name)
+    if item is None:
+        item = get_sword_choices(item_choice, wp_en, x, y, disp_name)
+    if item is None:
         equippable_component = Equippable(EquipmentSlots.MAIN_HAND, power_bonus=0)
-        item = Entity(x, y, '?', libtcod.black, 'ERROR WEAPON', equippable=equippable_component)
+        item = Entity(x, y, '?', libtcod.black, 'ERROR WEAPON'+item_choice, equippable=equippable_component)
 
     return item
